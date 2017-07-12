b9444c6e0033991cb51e23c41d98bf2bcdd7c133
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-12 12:33:40 +0200

    The scrollbar was the culprit

--- ChromeConsole/theme-extension/devtools.js
+++ ChromeConsole/theme-extension/devtools.js
@@ -9,5 +9,3 @@ if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > stableVersion) {
 xhr.open("GET", "/" + stylesheet, false);
 xhr.send();
 chrome.devtools.panels.applyStyleSheet(xhr.responseText);
-for (let i = 0; i < 10; i++) console.log(i)
-test.forEach(value => console.log(value))
